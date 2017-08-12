e41519aa4fc75c8f0eb506e21799da19af9c0359
raigomes <rai.gomes.ismart@gmail.com>
2017-08-12 17:19:07 -0300

    7.5 - Implementando Usuario e Senha

--- src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
+++ src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -46,6 +47,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
